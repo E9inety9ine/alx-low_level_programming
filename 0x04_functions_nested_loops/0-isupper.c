9c83d8bb9a9cd17fcdaf1b91b6e960eae8b188d7
Emekaedeh <emekaedeh2003@gmail.com>
2022-10-22 02:03:53 -0700

    task 0

---
+++ 0x04_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** 
+ * _isupper - checks if a character is uppercase
+ * @c: Variable text
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
+}
